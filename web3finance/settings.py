c813b9a5c0883cdd37ab95125ed8ee1f5e55f81a
MUNACHIW <munasom25@gmail.com>
2026-08-30 10:19:33 +0100

    debug true false

--- web3finance/settings.py
+++ web3finance/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-34%kwc^%#uj!ke@9u3mihwsp^at8axlq$%fp_lxye%8r!n%4k$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
